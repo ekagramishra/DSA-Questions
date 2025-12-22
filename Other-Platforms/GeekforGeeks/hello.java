9efac2474101aa286df349d0641b01d4b004cf5d
EKAGRA <ekagramishra24@gmail.com>
2025-12-22 18:09:35 +0530

    first geek for geeks question

---
+++ Other-Platforms/GeekforGeeks/hello.java
@@ -0,0 +1,30 @@
+/*
+i am just checking if i am able to push this code to github or not
+*/
+
+//question:
+/*
+
+Display the most famous phrase at the of beginning any programming language, "Hello World" (without Quotes).
+
+Example:
+
+Input:
+Output:
+Hello World 
+Explanation:
+Print Hello World on a single line.
+Your Task:  
+You don't need to read any input. Your task is to complete the function printHelloWorld() which does not take any input and prints 'Hello World'.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+*/
+
+class Solution {
+    static void printHelloWorld() {
+        // code here
+        System.out.println("Hello World");
+    }
+}
