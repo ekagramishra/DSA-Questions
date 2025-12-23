75c8f9d74351ab30e3bb39083ff29fbb0c58e782
EKAGRA MISHRA <ekagramishra24@gmail.com>
2025-12-23 19:35:25 +0530

    Time: 5 ms (73.92%), Space: 45.8 MB (72.38%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,14 +6,6 @@ class Solution {
         if(n<0){
             return false;
         }
-        // keeping it above made a greater impact
-
-        if(x==0){ //the test case i could not figure out
-            return true;
-        }
-        if(n%10==0){ //if the last digit turns out to be zero
-            return false;
-        }
         while(n>0){
             int d=n%10;
             rev= rev*10+d;
