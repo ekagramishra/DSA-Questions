f6efa239f840abf0387a681ec3aaa2447749aa64
EKAGRA MISHRA <ekagramishra24@gmail.com>
2026-03-09 17:36:31 +0530

    Time: 1 ms (79.21%), Space: 46.7 MB (61.31%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 1) return 1;
-
-        int k = 1;
-        
-        for (int i = 1; i < nums.length; i++) {
-
-            if (nums[i] != nums[k - 1]) {
-                nums[k] = nums[i]; 
-                k++; 
+        //thought process
+        /*
+        update nums when there is a unique element
+        skip when there's a repeated number
+        */ 
+        int i = 0;
+        for(int j = 1; j < nums.length; j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i] = nums[j];
             }
         }
-        return k;
+        return i + 1;
     }
 }
