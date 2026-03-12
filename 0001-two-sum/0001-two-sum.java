2d8d4df9345f07bcce667e53f9fda8924320e461
EKAGRA MISHRA <ekagramishra24@gmail.com>
2026-03-12 07:35:10 +0530

    Time: 34 ms (44.18%), Space: 46.9 MB (88.81%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int a=0;
+        int b=0 ;
+        for(int i=0;i<= nums.length-1;i++){
+            for(int j=0;j<=nums.length-1;j++){
+                if(i==j){
+                    break;
+                }
+                if(nums[i]+nums[j]== target){
+                    a= i;
+                    b= j;
+                    break;
+                }
+            }
+        }
+        return new int[]{a,b} ; //didnt know the return statement. 
+        // chatgpt'ed how to rerurn in this particular way
+    }
+}
