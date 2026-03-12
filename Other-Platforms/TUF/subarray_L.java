71d634059aaa03655a811d7956232169eed9b1d7
EKAGRA <ekagramishra24@gmail.com>
2026-03-12 07:23:23 +0530

    adding subarray

---
+++ Other-Platforms/TUF/subarray_L.java
@@ -0,0 +1,37 @@
+class subarray_L{
+    public int longestSubarray(int[] nums, int k){
+        int n= nums.length;
+        int count= 0;
+
+        for(int i= 0;i< n;i++){
+            int sum= 0;
+            for(int j= i;j< n;j++){
+                sum += nums[j];
+                if(sum == k){
+                    count= Math.max(count, (j - i + 1));
+                }
+            }
+        }
+        return count;
+    }
+}
+
+/*
+
+Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
+Example 1
+Input: nums = [10, 5, 2, 7, 1, 9],  k=15
+Output: 4
+
+Explanation:
+The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4. This sub-array starts at index 1 and ends at index 4, and the sum of its elements (5 + 2 + 7 + 1) equals 15. Therefore, the length of this sub-array is 4.
+
+Example 2
+Input: nums = [-3, 2, 1], k=6
+Output: 0
+
+Explanation:
+There is no sub-array in the array that sums to 6. Therefore, the output is 0.
+
+*/
