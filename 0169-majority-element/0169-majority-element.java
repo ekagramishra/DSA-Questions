4c13d6c9f37d9b83557b79d07f05550215c2b417
EKAGRA MISHRA <ekagramishra24@gmail.com>
2026-03-13 19:07:52 +0530

    Time: 2 ms (70.61%), Space: 55.8 MB (29.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,46 @@
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int n= nums.length;
-        int mustHave= n/2;
         int count= 0;
-        int hehe=0;
-        for(int i=0;i<=n-1;i++){
-            hehe=nums[i];
-            count=0;
-            for(int j=0;j<=n-1;j++){
-                if(nums[i]==nums[j]){
-                    ++count;
-                }
+        int candidate = 0;
+
+        for(int i=0;i<= nums.length-1;i++){
+            int num= nums[i];
+            if(count == 0){
+                candidate = num;
             }
-            if(count> mustHave){
-                break;
+            if(num == candidate){
+                count++;
+            } else{
+                count--;
             }
         }
-        return hehe;
+        return candidate;
     }
 }
+
+
+
+//this was my first approach
+
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n= nums.length;
+//         int mustHave= n/2;
+//         int count= 0;
+//         int hehe=0;
+//         for(int i=0;i<=n-1;i++){
+//             hehe=nums[i];
+//             count=0;
+//             for(int j=0;j<=n-1;j++){
+//                 if(nums[i]==nums[j]){
+//                     ++count;
+//                 }
+//             }
+//             if(count> mustHave){
+//                 break;
+//             }
+//         }
+//         return hehe;
+//     }
+// }
